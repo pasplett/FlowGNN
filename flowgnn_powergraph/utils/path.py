47938045a84a6c96535e2bdcdfb4c57e7763b98b
Pascal Plettenberg <plettenberg@uni-kassel.de>
2024-09-28 22:54:49 +0200

    FlowGNN code for cascading failure analysis

---
+++ flowgnn_powergraph/utils/path.py
@@ -0,0 +1,10 @@
+## Define the path to the data, model, logs, results, and colors
+#
+import os
+
+CKPT_ROOT = "../"
+DATA_DIR = CKPT_ROOT + "dataset_cascades/"
+MODEL_DIR = CKPT_ROOT + "experiment/"
+LOG_DIR = CKPT_ROOT + "logs/"
+RESULT_DIR = CKPT_ROOT + "results/"
+MASK_DIR = CKPT_ROOT + "mask/"
